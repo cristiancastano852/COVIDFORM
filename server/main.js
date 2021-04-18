6ade7217c047d606b5f2c8187c588fd1f4025e34
Alejandro_CristanchoM <alejandro.cristancho@udea.edu.co>
2021-04-17 22:01:00 -0500

    chat with io.socket implementation

--- server/main.js
+++ server/main.js
@@ -3,8 +3,27 @@ var app= express();
 var server= require('http').Server(app);
 var io= require('socket.io')(server);
 
-app.get('/',function(req,res){
-    res.status(200).send("Hola mundo");
+var messages=[{
+    id: 1,
+    text: "soy un mensaje",
+    author: "Alejandro"
+}];
+
+app.use(express.static('public'));
+
+app.get('/hello',function(req,res){
+    res.status(200).send("Hello world by Alejo");
+});
+
+io.on('connection', function(socket){
+    console.log('Mensaje por Socket');
+    socket.emit('messages',messages);
+
+    socket.on('new-message', function(data){
+        messages.push(data);
+        
+        io.sockets.emit('messages', messages);
+    });
 });
 
 server.listen(8080,function(){
