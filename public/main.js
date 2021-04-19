28d12071bfc4cc1279abe49f54e61a2650b7acee
cristian_castano1 <cristian.castano1@udea.edu.co>
2021-04-19 12:51:19 -0500

    functions added

--- public/main.js
+++ public/main.js
@@ -1,26 +1,43 @@
 var socket= io.connect('http://localhost:8080', {'forceNew':true});
 
+function setName(e){
+    window.location.replace('./temas.html');
+    localStorage.setItem('username', document.getElementById('username').value );
+
+    //socket.emit('new-username', username);
+    return false;
+    }
+
 socket.on('messages', function(data){
     console.log(data);
     render(data);
-})
+
+});
+
 
 function render(data) {
+       console.log(data);
         var html = data.map(function(elem,index){
+            console.log(data);
             return( `<div>
                 <strong>${elem.author}</strong>:
                 <em>${elem.text}</em>
              </div>`);
         }).join(" ");
-        
+        console.log(html);
         document.getElementById('messages').innerHTML = html;
-    }
+ }
 
-function addMessage(e){
+
+
+   function addMessage(e){
     var message={
-        author: document.getElementById('username').value,
+        author: localStorage.getItem('username'),
         text: document.getElementById('texto').value
     };
+
+
     socket.emit('new-message', message);
     return false;
 }
+
--- server/main.js
+++ server/main.js
@@ -1,31 +1,32 @@
 var express = require('express');
-var app= express(); 
-var server= require('http').Server(app);
-var io= require('socket.io')(server);
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var username = "";
 
-var messages=[{
-    id: 1,
-    text: "soy un mensaje",
-    author: "Alejandro"
+var messages = [{
+  id: 1,
+text: "Hola soy un mensaje",
+author: "Carlos Azaustre"
 }];
 
 app.use(express.static('public'));
 
-app.get('/hello',function(req,res){
-    res.status(200).send("Hello world by Alejo");
+app.get('/hello', function(req, res) {
+ res.status(200).send("Hello Worasasld!");
 });
 
-io.on('connection', function(socket){
-    console.log('Mensaje por Socket');
-    socket.emit('messages',messages);
+io.on('connection', function(socket) {
+  console.log('Alguien se ha conectado con Sockets');
+  socket.emit('messages', messages);
 
-    socket.on('new-message', function(data){
-        messages.push(data);
-        
-        io.sockets.emit('messages', messages);
-    });
+  socket.on('new-message', function(data) {
+    messages.push(data);
+    io.sockets.emit('messages', messages);
+  });
+
 });
 
-server.listen(8080,function(){
-    console.log("Running server in http://localhost:8080");
+server.listen(8080, function() {
+  console.log("Servidor corriendo en http://localhost:8080");
 });
