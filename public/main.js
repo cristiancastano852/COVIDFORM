fcd72891b635d0736feb9fd89904697cfcfd4a15
Alejandro_CristanchoM <alejandro.cristancho@udea.edu.co>
2021-04-19 19:12:35 -0500

    Functions added2

--- public/main.js
+++ public/main.js
@@ -3,40 +3,46 @@ var socket= io.connect('http://localhost:8080', {'forceNew':true});
 function setName(e){
     window.location.replace('./temas.html');
     localStorage.setItem('username', document.getElementById('username').value );
-
-    //socket.emit('new-username', username);
     return false;
     }
 
-socket.on('messages', function(data){
-    console.log(data);
-    render(data);
+function setView(e, view){
+        localStorage.setItem('viewToSave', view);
+        socket.emit('view', view)
+        //window.location.replace('./index'+view+'.html');
+        return false;
+}
 
+function getView(view){
+    return localStorage.getItem('viewToSave');
+}
+
+socket.on('messages', function(data){
+    render(data);
+    console.log(data);
 });
 
-
 function render(data) {
-       console.log(data);
-        var html = data.map(function(elem,index){
-            console.log(data);
+    var view= parseInt(localStorage.getItem('viewToSave'));
+    console.log(data);
+        var html = data[view-1].map(function(elem,index){
             return( `<div>
                 <strong>${elem.author}</strong>:
                 <em>${elem.text}</em>
              </div>`);
         }).join(" ");
-        console.log(html);
         document.getElementById('messages').innerHTML = html;
  }
 
 
 
-   function addMessage(e){
+function addMessage(e){
+    var view= localStorage.getItem('viewToSave');
     var message={
         author: localStorage.getItem('username'),
-        text: document.getElementById('texto').value
+        text: document.getElementById('texto').value,
+        view: view
     };
-
-
     socket.emit('new-message', message);
     return false;
 }
--- server/main.js
+++ server/main.js
@@ -2,13 +2,36 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var username = "";
 
 var messages = [{
   id: 1,
-text: "Hola soy un mensaje",
-author: "Carlos Azaustre"
+text: "En este foro se desarrolla el tema de lo que implica tener COVID-19 en casa",
+author: "Administrador",
+view:0
 }];
+
+var messages2 = [{
+  id: 1,
+text: "En este foro se desarrolla la pregunta ¿Tengo sintomas COVID-19?, no lo se tu dime",
+author: "Administrador",
+view:1
+}];
+
+var messages3 = [{
+  id: 1,
+text: "En este foro se desarrolla el tema de prevención contra el COVID-19",
+author: "Administrador",
+view:2
+}];
+
+var messages4 = [{
+  id: 1,
+text: "En este foro se desarrolla el tema sobre el estres que implica estar en confinamiento en época COVID-19",
+author: "Administrador",
+view:3
+}];
+
+var messagesArray = [messages,messages2,messages3,messages4];
 
 app.use(express.static('public'));
 
@@ -18,13 +41,15 @@ app.get('/hello', function(req, res) {
 
 io.on('connection', function(socket) {
   console.log('Alguien se ha conectado con Sockets');
-  socket.emit('messages', messages);
-
-  socket.on('new-message', function(data) {
-    messages.push(data);
-    io.sockets.emit('messages', messages);
+  socket.on('view',function(view){
+    socket.emit('messages', messagesArray);
   });
-
+socket.on('new-message', function(data) {
+    var view = parseInt(data.view);
+    messagesArray[view-1].push(data);
+    console.log(messagesArray[view-1],'se envió');
+    io.emit('messages', messagesArray);
+  });
 });
 
 server.listen(8080, function() {
